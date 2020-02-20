26c75d02dc2c17334416635e53c526b462a02555
cmhuang <cminghuang@gmail.com>
2020-02-20 09:24:55 +0800

    - Init.

---
+++ AccountingWeb/Controllers/BudgetController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AccountingWeb.Models;
+
+namespace AccountingWeb.Controllers
+{
+    public class BudgetController : Controller
+    {
+        public ActionResult Index()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Budgets budgets)
+        {
+            int res = 0;
+            if (!Int32.TryParse(budgets.YearMonth, out res))
+            {
+                ViewBag.Message = "YearMonth is Error";
+                return View();
+            }
+
+            String msg = "";
+            using (AccountingEntities ent = new AccountingEntities())
+            {
+                var record = ent.Budgets.FirstOrDefault(x => x.YearMonth.Equals(budgets.YearMonth));
+                if (record != null)
+                {
+                    record.Amount = budgets.Amount;
+                    msg = "Update OK";
+                }
+                else
+                {
+                    msg = "Add OK";
+                }
+
+                ent.Budgets.Add(budgets);
+                ent.SaveChanges();
+            }
+
+            ViewBag.Message = msg;
+            return View();
+
+        }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
---
+++ AccountingWebTests/UiTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.UI.HtmlControls;
+using Atata;
+using NUnit.Framework;
+
+namespace AccountingWebTests
+{
+    using _ = BudgetPage;
+
+    [Url("Index")] // Relative URL of the page.
+    public class BudgetPage : Page<_>
+    {
+        [FindById("YearMonth")] public TextInput<_> YearMonth { get; private set; }
+
+        [FindById("Amount")] public TextInput<_> Amount { get; private set; }
+
+        [FindById("Create")] public Button<_> Create { get; private set; }
+    }
+
+    [TestFixture]
+    public class UiTest
+    {
+        //if you cannot run web test, please check if you use the Chrome version 80 browser.
+        //if your version is less than 80, you may need to downgrade nuget package "Selenium.WebDriver.ChromeDriver" version to the appropriate version
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Find information about AtataContext set-up on https://atata.io/getting-started/#set-up
+            AtataContext.Configure()
+                .UseChrome()
+                //    WithArguments("start-maximized").
+                .UseBaseUrl("https://localhost:44314/Budget/")
+                .UseCulture("en-us").UseNUnitTestName()
+                .AddNUnitTestContextLogging().LogNUnitError()
+                .UseAssertionExceptionType<NUnit.Framework.AssertionException>()
+                .UseNUnitAggregateAssertionStrategy().Build();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AtataContext.Current?.CleanUp();
+        }
+
+        [Test]
+        public void create_budget()
+        {
+            // Go.ToUrl("https://dotblogs.com.tw/hatelove/1");
+            Go.To<BudgetPage>().YearMonth.Set("202001").Amount.Set("1000000").Create.Click();
+        }
+    }
+}
